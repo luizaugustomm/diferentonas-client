15c56e2e3aaba845367c83c56774dc77d12d6e53
Luiz Augusto Morais <luizaugustomm@gmail.com>
2016-04-10 19:40:01 -0300

    Primeira verão BETONA!

--- www/js/app.js
+++ www/js/app.js
@@ -7,52 +7,88 @@ angular.module('Diferentonas', ['ionic'])
 
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    $ionicPlatform.ready(function() {
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 
 .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
-        .state('index', {
-            url: '/',
-            templateUrl: 'templates/home.html',
-        })
-        .state('cards', {
-            url: '/cards',
-            templateUrl: 'templates/cards.html',
-            controller: 'CardsController'
-        })
+    .state('index', {
+        url: '/',
+        templateUrl: 'templates/home.html',
+    })
+    .state('cards', {
+        url: '/cards',
+        templateUrl: 'templates/cards.html',
+        controller: 'CardsController'
+    })
     $urlRouterProvider.otherwise('/');
 })
 
-
-.controller('CitySelectorController', function($scope, $http) {
-    $scope.cityInput = "";
-    $scope.selectedCity = null;
-
-    $scope.cities = [{"name": "João Pessoa"}, {"name": "Campina Grande"}, {"name": "Patos"}];
-
-    $scope.selectCity = function(city) {
-        $scope.selectedCity = city;
+.factory('City', function() {
+    return {
+        'info': null,
+        'similars': null
     };
 })
 
 
-.controller('CardsController', function($scope, $location) {
+.controller('CitySelectorController', function($scope, $http, $location, City) {
+    $scope.cityInput = "";
+    $scope.selectedCity = null;
+    $scope.isSelected = false;
+
+    $http.get('js/cities.json').success(function(data) {
+        $scope.cities = data;
+    });
+
+    $scope.selectCity = function(city) {
+        $scope.cityInput = city.nome;
+        $scope.selectedCity = city;
+        $scope.isSelected = !$scope.isSelected;
+    };
+
+    $scope.citySearched = function(city) {
+        $http.get('http://diferentonas.herokuapp.com/cidade/'.concat(city.id), {
+            headers: {'Access-Control-Allow-Origin': '*'}
+        }).success(function(data) {
+            City.info = data;
+        })
+        $http.get('http://diferentonas.herokuapp.com/cidade/'.concat(city.id).concat('/similares'), {
+            headers: {'Access-Control-Allow-Origin': '*'}
+        }).success(function(data) {
+            City.similars = data;
+        })
+        $scope.City = City;
+        $location.path("/cards");
+    }
+
+    $scope.cityCleared = function() {
+        $scope.cityInput = "";
+        $scope.selectedCity = null;
+        $scope.isSelected = false;
+    }
+})
+
+
+.controller('CardsController', function($scope, $location, City) {
+
+    $scope.City = City;
+
     $scope.cards = [
         {"topic": "Saúde", "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sodales facilisis nulla nec faucibus. Nunc sed fringilla nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla a arcu ornare, sodales nulla eu, dignissim nunc. Phasellus vulputate eros in elit malesuada tempus. Praesent tempus velit a arcu accumsan, id pellentesque ipsum ornare. Proin mi massa, egestas nec nisl sit amet, varius pulvinar quam."},
         {"topic": "Educação", "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sodales facilisis nulla nec faucibus. Nunc sed fringilla nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla a arcu ornare, sodales nulla eu, dignissim nunc. Phasellus vulputate eros in elit malesuada tempus. Praesent tempus velit a arcu accumsan, id pellentesque ipsum ornare. Proin mi massa, egestas nec nisl sit amet, varius pulvinar quam."},
