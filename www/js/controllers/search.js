e206e214a9838eb319b227ab70fe618db895c6ad
Jefferson Neves <jefferson.rpn@gmail.com>
2016-06-02 17:59:25 -0300

    A pesquisa pode ser feita ao submeter o form ou clicar no ícone da lupa

--- www/js/controllers/search.js
+++ www/js/controllers/search.js
@@ -1,18 +1,31 @@
 angular.module('Diferentonas')
 
-.controller('SearchCtrl', ['$http', '$location', 'City', function($http, $location, City) {
+.controller('SearchCtrl', ['$http', '$state', '$filter', 'City', function($http, $state, $filter, City) {
   var vm = this;
   vm.cityInput = "";
   vm.cities = [];
+  vm.selectedCity = {
+    "id": null,
+    "uf": null,
+    "nome": ""
+  };
   vm.isSearching = false;
 
   vm.startSearch = function() {
     vm.isSearching = true;
   }
-
   vm.selectCity = function(city) {
-    vm.cityInput = city.nome + ' - ' + city.uf;
+    vm.selectedCity = {
+      "id": city.id,
+      "uf": city.uf,
+      "nome": city.nome
+    };
   };
+  vm.search = function() {
+    var city = $filter('filter')(vm.cities, vm.selectedCity.nome)[0];
+    vm.selectCity(city);
+    $state.go('city', {'id_city': vm.selectedCity.id});
+  }
 
   $http.get('js/cities.json').success(function(data) {
     vm.cities = data;
