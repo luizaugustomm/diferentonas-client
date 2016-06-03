4d6f5d2cc5a061410595f94cb112bc9454af899f
Jefferson Neves <jefferson.rpn@gmail.com>
2016-06-03 11:25:30 -0300

    Update 2016-06-03T14:25:25.970Z

--- js/controllers/search.js
+++ js/controllers/search.js
@@ -1,19 +1,32 @@
 angular.module('Diferentonas')
 
-.controller('SearchCtrl', ['$http', '$location', '$ionicLoading', 'City', function($http, $location, $ionicLoading, City) {
+.controller('SearchCtrl', ['$http', '$state', '$filter', '$ionicLoading', 'City', function($http, $state, $filter, $ionicLoading, City) {
   var vm = this;
   vm.cityInput = "";
   vm.cities = [];
+  vm.selectedCity = {
+    "id": null,
+    "uf": null,
+    "nome": ""
+  };
   vm.isSearching = false;
 
   vm.startSearch = function() {
     vm.isSearching = true;
   }
-
   vm.selectCity = function(city) {
     $ionicLoading.show({ template: "<ion-spinner></ion-spinner>" });
-    vm.cityInput = city.nome + ' - ' + city.uf;
+    vm.selectedCity = {
+      "id": city.id,
+      "uf": city.uf,
+      "nome": city.nome
+    };
   };
+  vm.search = function() {
+    var city = $filter('filter')(vm.cities, vm.selectedCity.nome)[0];
+    vm.selectCity(city);
+    $state.go('city', {'id_city': vm.selectedCity.id});
+  }
 
   $http.get('js/cities.json').success(function(data) {
     vm.cities = data;
