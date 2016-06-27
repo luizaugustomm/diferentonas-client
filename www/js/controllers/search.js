fbfdbabd60ecab391c4b80a403d6cb24ad7ed9b4
Luiz Augusto Morais <luizaugustomm@gmail.com>
2016-06-27 10:51:11 -0300

    Limpa o campo de busca ao pesquisar por uma cidade

--- www/js/controllers/search.js
+++ www/js/controllers/search.js
@@ -1,14 +1,20 @@
 angular.module('Diferentonas')
 
 .controller('SearchCtrl', ['$http', '$state', '$filter', 'City', function($http, $state, $filter, City) {
+
+  var clearFields = function() {
+    vm.selectedCity = {
+      "id": null,
+      "uf": null,
+      "nome": ""
+    };
+  }
+
   var vm = this;
   vm.cityInput = "";
   vm.cities = [];
-  vm.selectedCity = {
-    "id": null,
-    "uf": null,
-    "nome": ""
-  };
+  vm.selectedCity = null;
+  clearFields();
   vm.isSearching = false;
 
   vm.startSearch = function() {
@@ -25,6 +31,7 @@ angular.module('Diferentonas')
     var city = $filter('filter')(vm.cities, vm.selectedCity.nome)[0];
     vm.selectCity(city);
     $state.go('city', {'id_city': vm.selectedCity.id});
+    clearFields();
   }
 
   $http.get('js/cities.json').success(function(data) {
