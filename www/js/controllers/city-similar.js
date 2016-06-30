b95eb9a5b623891db896deeae457353795b60db4
Jefferson Neves <jefferson.rpn@gmail.com>
2016-06-30 18:21:10 -0300

    Add tela de cidades semelhantes

---
+++ www/js/controllers/city-similar.js
@@ -0,0 +1,17 @@
+angular.module('Diferentonas')
+
+.controller('CitySimilarCtrl', ['$stateParams', '$http', '$ionicLoading', '$ionicScrollDelegate', 'City', function($stateParams, $http, $ionicLoading, $ionicScrollDelegate, City) {
+    $ionicLoading.show({ template: "<ion-spinner></ion-spinner>" });
+    var vm = this;
+    vm.CityResource = City;
+    vm.city = City.get({id: $stateParams.id}, function() {
+      vm.city.similars = City.similars.query({id: $stateParams.id}, function() {
+        $ionicLoading.hide();
+      }, function(error) {
+        $ionicLoading.hide();
+      });
+    }, function(error) {
+      $ionicLoading.hide();
+    });
+
+}]);
