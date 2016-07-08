341073c878dd1b3d2f6a6f373d4cb0eef0cc600f
Luiz Augusto Morais <luizaugustomm@gmail.com>
2016-07-08 11:47:57 -0300

    Implementada rolagem infinita na timeline do usuário

--- www/js/controllers/timeline.js
+++ www/js/controllers/timeline.js
@@ -3,35 +3,39 @@ angular.module('Diferentonas')
 .controller('TimelineCtrl', ['$ionicHistory', '$scope', '$http', '$ionicLoading', 'ionicToast', 'Timeline', function($ionicHistory, $scope , $http, $ionicLoading, ionicToast, Timeline) {
     $ionicLoading.show({ template: "<ion-spinner></ion-spinner>" });
     var vm = this;
-    vm.moreData = false;
-    vm.news = Timeline.query(function() {
-      if(vm.news.length == 0){
+    vm.moreData = true;
+    vm.np = 0;
+    vm.news = Timeline.query({npagina : vm.np}, function() {
+      if(vm.news.length === 0){
         moreData = false;
       }else{
         moreData = true;
       }
+      vm.np++;
       $ionicLoading.hide();
     }, function() {
       $ionicLoading.hide();
-      ionicToast.show("Não foi possível carregar mais informações, tente mais tarde.", 'center', false, 2500);
+      ionicToast.show("Não foi possível carregar mais informações, tente mais tarde.", 'center', false, 2000);
     });
 
     vm.loadMore = function() {
-      var items = Timeline.query(function() {
-        if(items.length == 0){
-          ionicToast.show("Não existem mais novidades", 'bottom', false, 2500);
+      var items = Timeline.query({npagina : vm.np}, function() {
+        console.log("Current page = " + vm.np);
+        if (items.length === 0){
+          ionicToast.show("Não existem mais novidades", 'bottom', false, 2000);
           moreData = false;
-        }else{
-          vm.news.push(items);
+        } else {
+          vm.news = vm.news.concat(items);
           moreData = true;
+          vm.np++;
         }
+        items = [];
+        $scope.$broadcast('scroll.infiniteScrollComplete');
         $ionicLoading.hide();
       }, function() {
         $ionicLoading.hide();
-        ionicToast.show("Não foi possível carregar mais informações, tente mais tarde.", 'center', false, 2500);
-      })
-      $scope.$broadcast('scroll.infiniteScrollComplete');
-      items = [];
+        ionicToast.show("Não foi possível carregar mais informações, tente mais tarde.", 'center', false, 2000);
+      });
     };
 
     vm.goBack = function() {
