dae8ddb609f802dee1de79547dfca0ea4b0575ac
IaraRibeiro <iara.prbr@gmail.com>
2016-07-20 17:28:25 -0300

    Adicionando o botão de seguir cidade

--- www/js/controllers/city-themes.js
+++ www/js/controllers/city-themes.js
@@ -31,4 +31,39 @@ angular.module('Diferentonas')
       $ionicHistory.goBack();
     }
 
+    vm.followCity= function() {
+      //adicionar chamada que faz o check do usuário seguir a iniciativa
+      /*
+      var api = ApiEndpoint.url + '/iniciativas/';
+      
+      if (vm.city.seguidaPeloRequisitante) {
+        $http.delete(api.concat(vm.city.id, "/inscritos"), vm.city.id, {
+            headers: {'Access-Control-Allow-Origin': '*'}
+        }).success(function(data) {
+            vm.city.seguidaPeloRequisitante = false;
+            $ionicLoading.hide();
+            ionicToast.show("Parou de seguir a cidade!", 'bottom', false, 2500);
+            })
+          .error(function(data) {
+            $ionicLoading.hide();
+            console.log(data);
+            ionicToast.show("Algo deu errado.", 'bottom', false, 2500);
+        });
+      } else {
+        $http.post(api.concat(vm.city.id, "/inscritos"), vm.city.id, {
+            headers: {'Access-Control-Allow-Origin': '*'}
+        }).success(function(data) {
+            vm.city.seguidaPeloRequisitante = true;
+            $ionicLoading.hide();
+            ionicToast.show("Está seguindo a cidade!", 'bottom', false, 2500);
+            })
+          .error(function(data) {
+            $ionicLoading.hide();
+            ionicToast.show("Algo deu errado.", 'bottom', false, 2500);
+        });
+      }
+      console.log("Cidade #" + vm.city.id + " está sendo seguida? " + vm.city.seguidaPeloRequisitante);
+      */
+    }
+
 }]);
